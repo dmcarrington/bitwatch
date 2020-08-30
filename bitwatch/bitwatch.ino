f110756ec7586f7958c34abb04d4df61328240b0
David Carrington <dmcarrington@googlemail.com>
2020-08-30 21:53:15 +0100

    started adding webserver

--- bitwatch/bitwatch.ino
+++ bitwatch/bitwatch.ino
@@ -23,6 +23,8 @@
 #include <soc/rtc.h>
 #include "esp_wifi.h"
 #include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
 #include <Electrum.h>
 #include "qrcode.h"
 #include "gui.h"
@@ -31,6 +33,11 @@
 #include "Bitcoin.h"
 #include "Hash.h"
 #include "seedwords.h"
+
+#define FILESYSTEM SPIFFS
+// You only need to format the filesystem once
+#define FORMAT_FILESYSTEM false
+#define DBG_OUTPUT_PORT Serial
 
 #define G_EVENT_VBUS_PLUGIN         _BV(0)
 #define G_EVENT_VBUS_REMOVE         _BV(1)
@@ -113,7 +120,201 @@ lv_obj_t * restoreInfo = NULL;
 lv_obj_t * restoreBtns = NULL;
 String theSeed = "";
 
+const char* ssid = "TALKTALK15D3E9";
+const char* password = "TRUDA3NG";
+const char* host = "esp32fs";
+WebServer server(80);
+String ipaddress = "";
+
 void pinmaker();
+
+//format bytes
+String formatBytes(size_t bytes) {
+  if (bytes < 1024) {
+    return String(bytes) + "B";
+  } else if (bytes < (1024 * 1024)) {
+    return String(bytes / 1024.0) + "KB";
+  } else if (bytes < (1024 * 1024 * 1024)) {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  } else {
+    return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
+  }
+}
+
+String getContentType(String filename) {
+  if (server.hasArg("download")) {
+    return "application/octet-stream";
+  } else if (filename.endsWith(".htm")) {
+    return "text/html";
+  } else if (filename.endsWith(".html")) {
+    return "text/html";
+  } else if (filename.endsWith(".css")) {
+    return "text/css";
+  } else if (filename.endsWith(".js")) {
+    return "application/javascript";
+  } else if (filename.endsWith(".png")) {
+    return "image/png";
+  } else if (filename.endsWith(".gif")) {
+    return "image/gif";
+  } else if (filename.endsWith(".jpg")) {
+    return "image/jpeg";
+  } else if (filename.endsWith(".ico")) {
+    return "image/x-icon";
+  } else if (filename.endsWith(".xml")) {
+    return "text/xml";
+  } else if (filename.endsWith(".pdf")) {
+    return "application/x-pdf";
+  } else if (filename.endsWith(".zip")) {
+    return "application/x-zip";
+  } else if (filename.endsWith(".gz")) {
+    return "application/x-gzip";
+  }
+  return "text/plain";
+}
+
+bool exists(String path){
+  bool yes = false;
+  fs::File file = FILESYSTEM.open(path, "r");
+  if(!file.isDirectory()){
+    yes = true;
+  }
+  file.close();
+  return yes;
+}
+
+bool handleFileRead(String path) {
+  DBG_OUTPUT_PORT.println("handleFileRead: " + path);
+  if (path.endsWith("/")) {
+    path += "index.htm";
+  }
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if (exists(pathWithGz) || exists(path)) {
+    if (exists(pathWithGz)) {
+      path += ".gz";
+    }
+    fs::File file = FILESYSTEM.open(path, "r");
+    server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
+void handleFileList() {
+  if (!server.hasArg("dir")) {
+    server.send(500, "text/plain", "BAD ARGS");
+    return;
+  }
+
+  String path = server.arg("dir");
+  DBG_OUTPUT_PORT.println("handleFileList: " + path);
+
+
+  fs::File root = FILESYSTEM.open(path);
+  path = String();
+
+  String output = "[";
+  if(root.isDirectory()){
+      fs::File file = root.openNextFile();
+      while(file){
+          if (output != "[") {
+            output += ',';
+          }
+          output += "{\"type\":\"";
+          output += (file.isDirectory()) ? "dir" : "file";
+          output += "\",\"name\":\"";
+          output += String(file.name()).substring(1);
+          output += "\"}";
+          file = root.openNextFile();
+      }
+  }
+  output += "]";
+  server.send(200, "text/json", output);
+}
+
+void startWebserver(void) {
+  //DBG_OUTPUT_PORT.begin(115200);
+  //DBG_OUTPUT_PORT.print("\n");
+  //DBG_OUTPUT_PORT.setDebugOutput(true);
+  //if (FORMAT_FILESYSTEM) FILESYSTEM.format();
+  //FILESYSTEM.begin();
+  {
+      fs::File root = FILESYSTEM.open("/");
+      fs::File file = root.openNextFile();
+      while(file){
+          String fileName = file.name();
+          size_t fileSize = file.size();
+          DBG_OUTPUT_PORT.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+          file = root.openNextFile();
+      }
+      DBG_OUTPUT_PORT.printf("\n");
+  }
+
+
+  //WIFI INIT
+  DBG_OUTPUT_PORT.printf("Connecting to %s\n", ssid);
+  if (String(WiFi.SSID()) != String(ssid)) {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+  }
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    DBG_OUTPUT_PORT.print(".");
+  }
+  DBG_OUTPUT_PORT.println("");
+  DBG_OUTPUT_PORT.print("Connected! IP address: ");
+  ipaddress = WiFi.localIP().toString();
+  DBG_OUTPUT_PORT.println(WiFi.localIP());
+
+  //MDNS.begin(host);
+  DBG_OUTPUT_PORT.print("Open http://");
+  DBG_OUTPUT_PORT.print(host);
+  DBG_OUTPUT_PORT.println(".local/edit to see the file browser");
+
+
+  //SERVER INIT
+  //list directory
+  server.on("/list", HTTP_GET, handleFileList);
+  //load editor
+  server.on("/edit", HTTP_GET, []() {
+    if (!handleFileRead("/edit.htm")) {
+      server.send(404, "text/plain", "FileNotFound");
+    }
+  });
+  //create file
+  //server.on("/edit", HTTP_PUT, handleFileCreate);
+  //delete file
+  //server.on("/edit", HTTP_DELETE, handleFileDelete);
+  //first callback is called after the request has ended with all parsed arguments
+  //second callback handles file uploads at that location
+  //server.on("/edit", HTTP_POST, []() {
+  //  server.send(200, "text/plain", "");
+  //}, handleFileUpload);
+
+  //called when the url is not defined here
+  //use it to load content from FILESYSTEM
+  server.onNotFound([]() {
+    if (!handleFileRead(server.uri())) {
+      server.send(404, "text/plain", "FileNotFound");
+    }
+  });
+
+  //get heap status, analog input value and all GPIO statuses in one json call
+  //server.on("/all", HTTP_GET, []() {
+  //  String json = "{";
+  //  json += "\"heap\":" + String(ESP.getFreeHeap());
+  //  json += ", \"analog\":" + String(analogRead(A0));
+  //  json += ", \"gpio\":" + String((uint32_t)(0));
+  //  json += "}";
+  //  server.send(200, "text/json", json);
+  //  json = String();
+  //});
+  server.begin();
+  DBG_OUTPUT_PORT.println("HTTP server started");
+
+}
 
 //========================================================================
 // Show an address as a QR code
@@ -139,7 +340,7 @@ void showAddress(String XXX){
  
   float scale = 4;
   int offset_x = 0;
-  int offset_y = 50;
+  int offset_y = 40;
 
   for (uint8_t y = 0; y < qrcode.size; y++) {
     for (uint8_t x = 0; x < qrcode.size; x++) {
@@ -184,12 +385,13 @@ void exportZpub() {
   showAddress(pubkey);
   ttgo->tft->setTextColor(TFT_BLACK);
   ttgo->tft->setCursor(0, 0);
-  ttgo->tft->setTextSize(3);
+  ttgo->tft->setTextSize(2);
   ttgo->tft->println(" EXPORT ZPUB");
   
   ttgo->tft->setTextSize(2);
-  ttgo->tft->setCursor(0, 220);
-  ttgo->tft->println(" Saved to SPIFFS");
+  ttgo->tft->setCursor(0, 200);
+  String msg = "Saved to \n" + ipaddress + "/bitwatch.txt";
+  ttgo->tft->println(msg.c_str());
   delay(5000);
 }
 
@@ -264,7 +466,9 @@ static void sign_tx_event_handler(lv_obj_t * obj, lv_event_t event)
       ttgo->tft->fillScreen(TFT_BLACK);
       ttgo->tft->setCursor(0, 100);
       ttgo->tft->setTextSize(2);
-      ttgo->tft->println(" Saved to SPIFFS");
+
+      String msg = "Saved to \n" + ipaddress + "/bitwatch.txt";
+      ttgo->tft->println(msg.c_str());
       ttgo->tft->println("");
 
       delay(3000);
@@ -616,6 +820,7 @@ static void confirmPin() {
       ttgo->tft->setTextSize(2);
       ttgo->tft->setTextColor(TFT_GREEN);
       ttgo->tft->print("Wallet setup!");
+      startWebserver();
       delay(3000);
       lv_obj_del(pinPad);
       lv_obj_del(pinTitle);
@@ -1142,6 +1347,8 @@ void loop()
         
         esp_restart();
       }
+
+      server.handleClient();
 
       lv_task_handler();
       delay(5);
