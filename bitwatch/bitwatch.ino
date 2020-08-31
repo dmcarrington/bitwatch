90e70fb72c4d312391bfc143746242a621b62875
David Carrington <dmcarrington@googlemail.com>
2020-08-31 11:52:26 +0100

    change to using wifi ap

--- bitwatch/bitwatch.ino
+++ bitwatch/bitwatch.ino
@@ -24,7 +24,7 @@
 #include "esp_wifi.h"
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <WebServer.h>
+//#include <WebServer.h>
 #include <Electrum.h>
 #include "qrcode.h"
 #include "gui.h"
@@ -33,6 +33,10 @@
 #include "Bitcoin.h"
 #include "Hash.h"
 #include "seedwords.h"
+
+//#include <WiFi.h>
+//#include <WiFiClient.h>
+#include <WiFiAP.h>
 
 #define FILESYSTEM SPIFFS
 // You only need to format the filesystem once
@@ -120,199 +124,27 @@ lv_obj_t * restoreInfo = NULL;
 lv_obj_t * restoreBtns = NULL;
 String theSeed = "";
 
-const char* ssid = "TALKTALK15D3E9";
-const char* password = "TRUDA3NG";
-const char* host = "esp32fs";
-WebServer server(80);
+const char *ssid = "yourAP";
+const char *password = "yourPassword";
+WiFiServer server(80);
 String ipaddress = "";
 
 void pinmaker();
 
-//format bytes
-String formatBytes(size_t bytes) {
-  if (bytes < 1024) {
-    return String(bytes) + "B";
-  } else if (bytes < (1024 * 1024)) {
-    return String(bytes / 1024.0) + "KB";
-  } else if (bytes < (1024 * 1024 * 1024)) {
-    return String(bytes / 1024.0 / 1024.0) + "MB";
-  } else {
-    return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
-  }
-}
-
-String getContentType(String filename) {
-  if (server.hasArg("download")) {
-    return "application/octet-stream";
-  } else if (filename.endsWith(".htm")) {
-    return "text/html";
-  } else if (filename.endsWith(".html")) {
-    return "text/html";
-  } else if (filename.endsWith(".css")) {
-    return "text/css";
-  } else if (filename.endsWith(".js")) {
-    return "application/javascript";
-  } else if (filename.endsWith(".png")) {
-    return "image/png";
-  } else if (filename.endsWith(".gif")) {
-    return "image/gif";
-  } else if (filename.endsWith(".jpg")) {
-    return "image/jpeg";
-  } else if (filename.endsWith(".ico")) {
-    return "image/x-icon";
-  } else if (filename.endsWith(".xml")) {
-    return "text/xml";
-  } else if (filename.endsWith(".pdf")) {
-    return "application/x-pdf";
-  } else if (filename.endsWith(".zip")) {
-    return "application/x-zip";
-  } else if (filename.endsWith(".gz")) {
-    return "application/x-gzip";
-  }
-  return "text/plain";
-}
-
-bool exists(String path){
-  bool yes = false;
-  fs::File file = FILESYSTEM.open(path, "r");
-  if(!file.isDirectory()){
-    yes = true;
-  }
-  file.close();
-  return yes;
-}
-
-bool handleFileRead(String path) {
-  DBG_OUTPUT_PORT.println("handleFileRead: " + path);
-  if (path.endsWith("/")) {
-    path += "index.htm";
-  }
-  String contentType = getContentType(path);
-  String pathWithGz = path + ".gz";
-  if (exists(pathWithGz) || exists(path)) {
-    if (exists(pathWithGz)) {
-      path += ".gz";
-    }
-    fs::File file = FILESYSTEM.open(path, "r");
-    server.streamFile(file, contentType);
-    file.close();
-    return true;
-  }
-  return false;
-}
-
-void handleFileList() {
-  if (!server.hasArg("dir")) {
-    server.send(500, "text/plain", "BAD ARGS");
-    return;
-  }
-
-  String path = server.arg("dir");
-  DBG_OUTPUT_PORT.println("handleFileList: " + path);
-
-
-  fs::File root = FILESYSTEM.open(path);
-  path = String();
-
-  String output = "[";
-  if(root.isDirectory()){
-      fs::File file = root.openNextFile();
-      while(file){
-          if (output != "[") {
-            output += ',';
-          }
-          output += "{\"type\":\"";
-          output += (file.isDirectory()) ? "dir" : "file";
-          output += "\",\"name\":\"";
-          output += String(file.name()).substring(1);
-          output += "\"}";
-          file = root.openNextFile();
-      }
-  }
-  output += "]";
-  server.send(200, "text/json", output);
-}
-
 void startWebserver(void) {
-  //DBG_OUTPUT_PORT.begin(115200);
-  //DBG_OUTPUT_PORT.print("\n");
-  //DBG_OUTPUT_PORT.setDebugOutput(true);
-  //if (FORMAT_FILESYSTEM) FILESYSTEM.format();
-  //FILESYSTEM.begin();
-  {
-      fs::File root = FILESYSTEM.open("/");
-      fs::File file = root.openNextFile();
-      while(file){
-          String fileName = file.name();
-          size_t fileSize = file.size();
-          DBG_OUTPUT_PORT.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
-          file = root.openNextFile();
-      }
-      DBG_OUTPUT_PORT.printf("\n");
-  }
+  //Serial.begin(115200);
+  Serial.println();
+  Serial.println("Configuring access point...");
 
-
-  //WIFI INIT
-  DBG_OUTPUT_PORT.printf("Connecting to %s\n", ssid);
-  if (String(WiFi.SSID()) != String(ssid)) {
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-  }
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    DBG_OUTPUT_PORT.print(".");
-  }
-  DBG_OUTPUT_PORT.println("");
-  DBG_OUTPUT_PORT.print("Connected! IP address: ");
-  ipaddress = WiFi.localIP().toString();
-  DBG_OUTPUT_PORT.println(WiFi.localIP());
-
-  //MDNS.begin(host);
-  DBG_OUTPUT_PORT.print("Open http://");
-  DBG_OUTPUT_PORT.print(host);
-  DBG_OUTPUT_PORT.println(".local/edit to see the file browser");
-
-
-  //SERVER INIT
-  //list directory
-  server.on("/list", HTTP_GET, handleFileList);
-  //load editor
-  server.on("/edit", HTTP_GET, []() {
-    if (!handleFileRead("/edit.htm")) {
-      server.send(404, "text/plain", "FileNotFound");
-    }
-  });
-  //create file
-  //server.on("/edit", HTTP_PUT, handleFileCreate);
-  //delete file
-  //server.on("/edit", HTTP_DELETE, handleFileDelete);
-  //first callback is called after the request has ended with all parsed arguments
-  //second callback handles file uploads at that location
-  //server.on("/edit", HTTP_POST, []() {
-  //  server.send(200, "text/plain", "");
-  //}, handleFileUpload);
-
-  //called when the url is not defined here
-  //use it to load content from FILESYSTEM
-  server.onNotFound([]() {
-    if (!handleFileRead(server.uri())) {
-      server.send(404, "text/plain", "FileNotFound");
-    }
-  });
-
-  //get heap status, analog input value and all GPIO statuses in one json call
-  //server.on("/all", HTTP_GET, []() {
-  //  String json = "{";
-  //  json += "\"heap\":" + String(ESP.getFreeHeap());
-  //  json += ", \"analog\":" + String(analogRead(A0));
-  //  json += ", \"gpio\":" + String((uint32_t)(0));
-  //  json += "}";
-  //  server.send(200, "text/json", json);
-  //  json = String();
-  //});
+  // You can remove the password parameter if you want the AP to be open.
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  ipaddress = myIP.toString();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
   server.begin();
-  DBG_OUTPUT_PORT.println("HTTP server started");
+
+  Serial.println("Server started");
 
 }
 
@@ -1348,7 +1180,38 @@ void loop()
         esp_restart();
       }
 
-      server.handleClient();
+      WiFiClient client = server.available();   // listen for incoming clients
+
+      if (client) {                             // if you get a client,
+        Serial.println("New Client.");           // print a message out the serial port
+        String currentLine = "";                // make a String to hold incoming data from the client
+        while (client.connected()) {            // loop while the client's connected
+          if (client.available()) {             // if there's bytes to read from the client,
+            char c = client.read();             // read a byte, then
+            Serial.write(c);                    // print it out the serial monitor
+            if (c == '\n') {                    // if the byte is a newline character
+    
+              // if the current line is blank, you got two newline characters in a row.
+              // that's the end of the client HTTP request, so send a response:
+              if (currentLine.length() == 0) {
+                // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                // and a content-type so the client knows what's coming, then a blank line:
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-type:text/html");
+                client.println();
+                // print content of bitwatch.txt
+                fs::File file = SPIFFS.open("/bitwatch.txt", FILE_READ);
+                String bitwatch = file.readStringUntil('\n');
+                client.println(bitwatch);
+                file.close();
+              }
+            }
+          }
+        }
+        // close the connection:
+        client.stop();
+        Serial.println("Client Disconnected.");
+      }
 
       lv_task_handler();
       delay(5);
